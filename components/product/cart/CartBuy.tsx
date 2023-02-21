0279b72bfdb1c18e23d2190919377088c4de225d
MOH DEDE FAZRI <fazrithe@gmail.com>
2023-02-21 11:36:42 +0700

    update cart

---
+++ components/product/cart/CartBuy.tsx
@@ -0,0 +1,47 @@
+import { Card, Hidden, CardContent, Divider, Grid, Paper, Typography } from "@mui/material";
+
+export default function CartBuy(){
+    return(
+        <>
+            <Hidden only="xs">
+            <Card sx={{ minWidth: 275, position: "fixed" }}>
+                <CardContent>
+                    <Paper>
+                        <Grid container padding={1}>
+                            <Grid item xs={2} sm={1}>
+                                <Typography color="secondary">%</Typography>
+                            </Grid>
+                            <Grid item xs={6} sm={8}>
+                                <Typography color="secondary">Lebih hemat pakai promo</Typography>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                    <hr/>
+                    <Grid container>
+                        <Grid item>
+                            <Typography variant="h6">Ringkasan Belanja</Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid container paddingTop={1}>
+                        <Grid item xs={4} sm={8}>
+                            <Typography>Total Harga (1 Brang)</Typography>
+                        </Grid>
+                        <Grid item xs={4} sm={4}>
+                            <Typography>Rp. 70.000</Typography>
+                        </Grid>
+                    </Grid>
+                    <hr/>
+                    <Grid container>
+                        <Grid item xs={4} sm={8}>
+                            <Typography fontWeight="bold">Total Harga</Typography>
+                        </Grid>
+                        <Grid item xs={4} sm={4}>
+                            <Typography fontWeight="bold">Rp. 70.000</Typography>
+                        </Grid>
+                    </Grid>
+                </CardContent>
+            </Card>
+            </Hidden>
+        </>
+    )
+}
--- components/product/cart/CartDetail.tsx
+++ components/product/cart/CartDetail.tsx
@@ -87,6 +87,118 @@ export default function CartDetail(){
                 </Grid>
             </Grid>
         <Divider/>
+        <Grid container>
+                <Grid item xs={2} sm={0.7} paddingTop={1}>
+                    <Checkbox/>
+                </Grid>
+                <Grid item xs={6} sm={8} paddingTop={1.2}>
+                    <Typography fontWeight="bold">Aji Motor</Typography>
+                    <Typography variant="caption" color="text.secondary">Jakarta Barat</Typography>
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs={2} sm={0.7} paddingTop={1}>
+                    <Checkbox/>
+                </Grid>
+                <Grid item xs={4} sm={2} paddingTop={1}>
+                    <Image src={ImageProduct}  alt="Helem KYT" width={100} />
+                </Grid>
+                <Grid item xs={6} sm={9} paddingTop={1.2}>
+                    <Grid container>
+                        <Grid item xs={8} sm={10}>
+                            <Typography>Helem KYT</Typography>
+                            <Typography variant="caption" color="text.secondary">Putih</Typography>
+                        </Grid>      
+                        <Grid item xs={3} sm={2}>
+                            |<DeleteOutlinedIcon/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid container paddingTop={1}>
+                <Grid item xs={2} sm={0.7}></Grid>
+                <Grid item xs={3} sm={7}>
+                    <Typography variant="caption" color="secondary">Tulis Catatan Catatan</Typography>
+                </Grid>
+                <Grid item xs={6} sm={4} paddingBottom={1}>
+                    <Grid container>
+                        <Grid item xs={8} sm={8}>
+                            <Grid container spacing={3}>
+                                <Grid item xs={7} sm={5}>
+                                    <Button variant="outlined" size="small" color="secondary" onClick={decrementCount}>
+                                        <Typography fontWeight="bold">-</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={1} sm={1} padding={1}>
+                                    <Typography fontWeight="bold" color="secondary">{count}</Typography>
+                                </Grid>
+                                <Grid item xs={2} sm={5}>
+                                    <Button variant="outlined" size="small" color="secondary" onClick={incrementCount}>
+                                        <Typography fontWeight="bold">+</Typography>
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        <Divider/>
+        <Grid container>
+                <Grid item xs={2} sm={0.7} paddingTop={1}>
+                    <Checkbox/>
+                </Grid>
+                <Grid item xs={6} sm={8} paddingTop={1.2}>
+                    <Typography fontWeight="bold">Aji Motor</Typography>
+                    <Typography variant="caption" color="text.secondary">Jakarta Barat</Typography>
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item xs={2} sm={0.7} paddingTop={1}>
+                    <Checkbox/>
+                </Grid>
+                <Grid item xs={4} sm={2} paddingTop={1}>
+                    <Image src={ImageProduct}  alt="Helem KYT" width={100} />
+                </Grid>
+                <Grid item xs={6} sm={9} paddingTop={1.2}>
+                    <Grid container>
+                        <Grid item xs={8} sm={10}>
+                            <Typography>Helem KYT</Typography>
+                            <Typography variant="caption" color="text.secondary">Putih</Typography>
+                        </Grid>      
+                        <Grid item xs={3} sm={2}>
+                            |<DeleteOutlinedIcon/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid container paddingTop={1}>
+                <Grid item xs={2} sm={0.7}></Grid>
+                <Grid item xs={3} sm={7}>
+                    <Typography variant="caption" color="secondary">Tulis Catatan Catatan</Typography>
+                </Grid>
+                <Grid item xs={6} sm={4} paddingBottom={1}>
+                    <Grid container>
+                        <Grid item xs={8} sm={8}>
+                            <Grid container spacing={3}>
+                                <Grid item xs={7} sm={5}>
+                                    <Button variant="outlined" size="small" color="secondary" onClick={decrementCount}>
+                                        <Typography fontWeight="bold">-</Typography>
+                                    </Button>
+                                </Grid>
+                                <Grid item xs={1} sm={1} padding={1}>
+                                    <Typography fontWeight="bold" color="secondary">{count}</Typography>
+                                </Grid>
+                                <Grid item xs={2} sm={5}>
+                                    <Button variant="outlined" size="small" color="secondary" onClick={incrementCount}>
+                                        <Typography fontWeight="bold">+</Typography>
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        <Divider/>
         </>
     )
 }
